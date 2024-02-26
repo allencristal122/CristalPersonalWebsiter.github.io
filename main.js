b4553554896c0fc8243c0a0e16751223c9c1ce88
allencristal122 <153245875+allencristal122@users.noreply.github.com>
2024-02-26 17:44:08 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text", {
+    // Array of strings to be typed out
+    strings:["Student.", "Video Editor.", "Your Future Web Developer."],
+    // Typing speed in milliseconds
+    typeSpeed: 100,
+    // Speed to erase previously typed characters in milliseconds
+    backSpeed: 100,
+    // Time delay in milliseconds before starting to erase
+    backDelay:1000,
+    // Whether to loop through the strings indefinitely
+    loop: true
+})
---
+++ main1.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const nav = document.querySelector('.navigation');
+    const hamburger = document.querySelector('.hamburger');
+    nav.classList.toggle('active');
+    hamburger.classList.toggle('active');
+}
